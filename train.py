09fb54fddc2c6c9d6ea2fa33099ed0efcb954eb8
Phat Dat Nguyen <phatdatn@berkeley.edu>
2021-03-28 00:50:17 -0700

    updated train

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ def main():
     if use_wandb:
         wandb.init(project="182-vision")
 
-    device='cuda'
+    device='cpu'
 
     data_dir = pathlib.Path('./data/tiny-imagenet-200')
     image_count = len(list(data_dir.glob('**/*.JPEG')))
@@ -33,7 +33,8 @@ def main():
     batch_size = 32
     im_height = 64
     im_width = 64
-    num_epochs = 1
+    epochs = 1
+    print_every = 10
 
     data_transforms = transforms.Compose([
         transforms.ToTensor(),
@@ -49,29 +50,11 @@ def main():
 
     model = torchvision.models.resnet50(pretrained=True)
     model.fc = nn.Linear(in_features=2048, out_features=200, bias=True)
-    print(model)
     optimizer = torch.optim.Adam(model.parameters())
     criterion = nn.CrossEntropyLoss()
-    # for i in range(num_epochs):
-    #     train_total, train_correct = 0,0
-    #     for idx, (inputs, targets) in enumerate(train_loader):
-    #         optim.zero_grad()
-    #         outputs = model(inputs)
-    #         loss = criterion(outputs, targets)
-    #         loss.backward()
-    #         optim.step()
-    #         _, predicted = outputs.max(1)
-    #         train_total += targets.size(0)
-    #         train_correct += predicted.eq(targets).sum().item()
-    #         print("\r", end='')
-    #         print(f'training {100 * idx / len(train_loader):.2f}%: {train_correct / train_total:.3f}', end='')
-    #     torch.save({
-    #         'net': model.state_dict(),
-    #     }, 'latest.pt')
-    epochs = 1
+
     steps = 0
     train_loss = 0
-    print_every = 10
     train_losses, val_losses = [], []
     for epoch in range(epochs):
         for inputs, labels in train_loader:
@@ -116,6 +99,10 @@ def main():
         torch.save({
             'net': model.state_dict(),
         }, 'latest.pt')
+        plt.plot(train_losses, label='Training loss')
+        plt.plot(test_losses, label='Validation loss')
+        plt.legend(frameon=False)
+        plt.savefig('loss.png')
 
 if __name__ == '__main__':
     main()
